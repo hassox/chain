4fb22c43d4d40f4251d5a51c50432f4935f9831f
Daniel Neighman <has.sox@gmail.com>
2009-12-17 06:59:56 +1100

    A midly working example

---
+++ examples/simple_chain.js
@@ -0,0 +1,22 @@
+process.mixin(require("sys"));
+process.mixin(require('./../lib/link'));
+
+var Responder = new Link.App.Standard("Responder", {
+  on : {
+    request  : function(env){
+      puts("Recieved Request in the responder");
+      this.pass(env);
+    },
+    response : function(env){
+      env.body = "Hi There";
+      var res = env.response;
+      res.sendHeader(env.status, env.headers);
+      res.sendBody(env.body);
+      res.finish();
+    }
+  }
+});
+
+Link.Builder.chain(Responder, [new Link.App.LoopBack("loopback")]);
+
+Link.run(Responder);
