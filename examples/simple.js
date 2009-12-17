5eb6da213c64f56838816264f1f53858f16ed8ce
Joshua Hull <joshbuddy@gmail.com>
2009-12-17 18:36:40 -0500

    removed ensure callbacks.

---
+++ examples/simple.js
@@ -0,0 +1,20 @@
+process.mixin(require("sys"));
+process.mixin(require('./../lib/link'));
+
+app = Link.Builder.chain([
+  Link.Links.Responder,
+  function(env) {
+    
+    env.finish(function() {
+      puts('all done!')
+    })
+    
+    env.call(function() {
+      env.body += 'hey guys'
+      env.done()
+    })
+  }
+])
+
+Link.run(app);
+
