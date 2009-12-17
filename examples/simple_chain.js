ab5b57e4f45128ebb4c5fb463d41feacf5afa0a6
Joshua Hull <joshbuddy@gmail.com>
2009-12-17 17:07:34 -0500

    lower the B, drop the on. namespace

--- examples/simple_chain.js
+++ examples/simple_chain.js
@@ -13,7 +13,7 @@ var Auditor = {
   }
 }
 
-Link.addGlobalCallBack(function(app){
+Link.addGlobalCallback(function(app){
   app.addListener("audit",function(env){
     Auditor.handle(env);
   });
@@ -21,49 +21,43 @@ Link.addGlobalCallBack(function(app){
 
 
 var Responder = new Link.App("Responder", {
-  on : {
-    request  : function(env){
-      var self = this;
-      env.callBack(function(){
-        self.on.response(env);
-      });
+  onRequest : function(env){
+    var self = this;
+    env.callback(function(){
+      self.onResponse(env);
+    });
 
-      this.emit("audit", env, "Some Message from the responder");
-      // route
-      if(env.request.uri.path == "/test"){
-        this.nextApp.emit("request", env);
-      } else {
-        SomeEndPoint.emit("request", env);
-      }
-    },
-
-    response : function(env){
-      var res = env.response;
-      res.sendHeader(env.status, env.headers);
-      res.sendBody(env.body);
-      res.finish();
-      env.complete();
+    this.emit("audit", env, "Some Message from the responder");
+    // route
+    if(env.request.uri.path == "/test"){
+      this.nextApp.emit("request", env);
+    } else {
+      SomeEndPoint.emit("request", env);
     }
+  },
+
+  onResponse : function(env){
+    var res = env.response;
+    res.sendHeader(env.status, env.headers);
+    res.sendBody(env.body);
+    res.finish();
+    env.complete();
   }
 });
 
 var SomeEndPoint = new Link.App("SomeEndPoint", {
-  on : {
-    request : function(env){
-      env.body += "I'm In " + this.name;
-      env.done();
-      this.emit("audit", env, "Hi, I'm in SomeEndPoint and I've got somethign to say");
-    }
+  onRequest : function(env){
+    env.body += "I'm In " + this.name;
+    env.done();
+    this.emit("audit", env, "Hi, I'm in SomeEndPoint and I've got somethign to say");
   }
 })
 
 var EndPoint = new Link.App("EndPoint", {
-  on : {
-    request : function(env){
-      env.body += "<br/>I'm in the endpoint now!";
-      env.headers["Content-Type"] = "text/plain";
-      env.done();
-    }
+  onRequest : function(env){
+    env.body += "<br/>I'm in the endpoint now!";
+    env.headers["Content-Type"] = "text/plain";
+    env.done();
   }
 });
 
