d7ff58aa29dbe0da8dc383d5b9b4e7701de830aa
Daniel Neighman <has.sox@gmail.com>
2009-12-17 19:35:52 +1100

    Gets 'er workin'

--- examples/simple_chain.js
+++ examples/simple_chain.js
@@ -5,12 +5,15 @@ var Responder = new Link.App.Standard("Responder", {
   on : {
     request  : function(env){
       var self = this;
-      env.body += "In Request on Responder";
       env.callBack(function(){
         self.on.response(env);
       });
-
-      this.pass(env);
+      // route
+      if(env.request.uri.path == "/test"){
+        this.nextApp.emit("request", env);
+      } else {
+        SomeEndPoint.emit("request", env);
+      }
     },
 
     response : function(env){
@@ -22,5 +25,26 @@ var Responder = new Link.App.Standard("Responder", {
     }
   }
 });
+
+var SomeEndPoint = new Link.App.Standard("SomeEndPoint", {
+  on : {
+    request : function(env){
+      env.body += "I'm In " + this.name;
+      env.done();
+    }
+  }
+})
+
+var EndPoint = new Link.App.Standard("EndPoint", {
+  on : {
+    request : function(env){
+      env.body += "<br/>I'm in the endpoint now!";
+      env.headers["Content-Type"] = "text/plain";
+      env.done();
+    }
+  }
+});
+
+Link.Builder.chain(Responder, [EndPoint]);
 
 Link.run(Responder);
