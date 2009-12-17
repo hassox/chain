b8476eddc0cfbf2dce027390d778d45e6448cca5
Daniel Neighman <has.sox@gmail.com>
2009-12-18 08:49:06 +1100

    Adds an environment file and updates the example

--- examples/simple_chain.js
+++ examples/simple_chain.js
@@ -1,13 +1,34 @@
 process.mixin(require("sys"));
 process.mixin(require('./../lib/link'));
 
-var Responder = new Link.App.Standard("Responder", {
+var Auditor = {
+  handle : function(env){
+    puts("Auditing the environment with uri " + env.request.uri.path);
+    puts(inspect(arguments));
+    args = Array.prototype.slice.call(arguments)
+    args.shift();
+    while(message = args.shift()){
+      puts("   " + message);
+    }
+  }
+}
+
+Link.addGlobalCallBack(function(app){
+  app.addListener("audit",function(env){
+    Auditor.handle(env);
+  });
+});
+
+
+var Responder = new Link.App("Responder", {
   on : {
     request  : function(env){
       var self = this;
       env.callBack(function(){
         self.on.response(env);
       });
+
+      this.emit("audit", env, "Some Message from the responder");
       // route
       if(env.request.uri.path == "/test"){
         this.nextApp.emit("request", env);
@@ -26,16 +47,17 @@ var Responder = new Link.App.Standard("Responder", {
   }
 });
 
-var SomeEndPoint = new Link.App.Standard("SomeEndPoint", {
+var SomeEndPoint = new Link.App("SomeEndPoint", {
   on : {
     request : function(env){
       env.body += "I'm In " + this.name;
       env.done();
+      this.emit("audit", env, "Hi, I'm in SomeEndPoint and I've got somethign to say");
     }
   }
 })
 
-var EndPoint = new Link.App.Standard("EndPoint", {
+var EndPoint = new Link.App("EndPoint", {
   on : {
     request : function(env){
       env.body += "<br/>I'm in the endpoint now!";
@@ -44,6 +66,7 @@ var EndPoint = new Link.App.Standard("EndPoint", {
     }
   }
 });
+
 
 Link.Builder.chain(Responder, [EndPoint]);
 
