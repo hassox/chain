33e9d3cca8107aeddb59a4e79f7ea116ddf927bc
Joshua Hull <joshbuddy@gmail.com>
2009-12-18 03:04:04 -0500

    setTimeout 0 was so slow, to be unsuable. if someone else hs a better way, let them speak

--- examples/simple.js
+++ examples/simple.js
@@ -4,15 +4,16 @@ process.mixin(require('./../lib/link'));
 app = Link.Builder.chain([
   Link.Links.Responder,
   function(env) {
-    
+
     env.finish(function() {
       puts('all done!')
     })
-    
+
     env.call(function() {
       env.body += 'hey guys'
       env.done()
     })
+
   }
 ])
 
