e3f0873ad2d0175b1b0f931fa604155d5c5d828b
Daniel Neighman <has.sox@gmail.com>
2010-01-25 22:51:06 +1100

    Simple request object

--- examples/simple.js
+++ examples/simple.js
@@ -12,14 +12,26 @@ will return
 */
 
 builder = new Chain.Builder()
-
+var counter = 0;
 builder
   .use(function(env) {
+      //var requestNumber = 0 + ++counter;
+      var buffer = [];
+
+      //sys.puts("Request: " + requestNumber);
+
+      env.request.addListener("body", function(data){
+        buffer.push(data);
+      })
+
+      env.request.addListener("complete", function(){
+       // sys.puts("Complete: " + requestNumber);
         env.headers['content-type'] = 'text/html';
-        env.body = "<h1>Hello jello world</h1>";
+        env.body = "<h1>Hello jello world</h1>" + buffer.join("");
         env.done();
-      }
-    );
+      })
+
+    });
 
 Chain.run(builder.build());
 
